08c9e2ef033777dee72c33d1dcd58e8bf6f8576e
exhaustion666 <crowford-1337@mail.ru>
2024-11-22 16:45:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from load_image import load_image
 from parse_agrs import parse_args
 
 def main():
-    input_image_path, output_image_path = parse_args()
-    image = load_image(input_image_path)
-    print(f"Image size: {image.shape[1]} x {image.shape[0]} (Width x Height)")
-    plot_histogram(image)
-    inverted_image = invert_colors(image)
-    display_image(image, title='Original Image')
-    display_image(inverted_image, title='Inverted Image')
     try:
+        input_image_path, output_image_path = parse_args()
+        image = load_image(input_image_path)
+        print(f"Image size: {image.shape[1]} x {image.shape[0]} (Width x Height)")
+        plot_histogram(image)
+        inverted_image = invert_colors(image)
+        display_image(image, title='Original Image')
+        display_image(inverted_image, title='Inverted Image')
         cv2.imwrite(output_image_path, inverted_image)
         print(f"Inverted image saved to {output_image_path}")
     except Exception as e:
